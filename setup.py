4782c4bcfa0aa7ee3acf956bf97b4adee871a838
Brandon Thayer <brandon.lewis.thayer@gmail.com>
2020-10-11 11:30:12 -0700

    Initial swing at a setup.py file.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='disquip-bot',
+    version='0.1.0',
+    url='https://github.com/blthayer/disquip-bot',
+    license='MIT',
+    author='Brandon Thayer',
+    author_email='brandon.lewis.thayer@gmail.com',
+    description='An easy-to-use Discord soundboard bot. BYO audio files ' 
+                'and quip away!',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=find_packages(".", exclude=['tests', '*.tests', '*.tests.*']),
+    install_requires=['attr', 'discord.py[voice]', 'tabulate'],
+    python_requires='>=3.7',
+    setup_requires=['wheel']
+)
