43f6b09a174f7d3aff54326dc1927317dd3ef02e
Brandon Thayer <brandon.lewis.thayer@gmail.com>
2020-10-24 11:34:43 -0700

    Updating disquip.ini and __main__ to leverage the new normalization.

--- disquip/__main__.py
+++ disquip/__main__.py
@@ -3,12 +3,14 @@
 # Standard library:
 import configparser
 import logging
+import os
 import re
 from typing import List
 
 # disquip.
 from disquip.bot import BotHelper, DisQuipBot
 from disquip.discover import AudioCollection
+from disquip.normalize import normalize
 
 
 def split(str_in: str) -> List[str]:
@@ -26,9 +28,36 @@ def main():
     # Get a list of audio extensions.
     audio_extensions = split(dqc['audio_extensions'])
 
+    # Extract the ffmpeg_path.
+    ffmpeg_path = dqc['ffmpeg_path']
+
+    # Pre-normalize files.
+    if int(dqc['pre_normalize']):
+        # Create a sibling audio directory called "normalized."
+        audio_directory = dqc['audio_directory'] + '_normalized'
+        try:
+            os.mkdir(audio_directory)
+        except FileExistsError:
+            # No big deal if the directory already exists.
+            pass
+
+        # Normalize each directory.
+        for sub_dir in os.scandir(dqc['audio_directory']):
+            # Skip non-directories.
+            if not sub_dir.is_dir():
+                continue
+
+            # Normalize all the files in the directory and put them in
+            # the new normalized directory.
+            normalize(in_dir=sub_dir.path,
+                      out_dir=os.path.join(audio_directory, sub_dir.name),
+                      extensions=audio_extensions, ffmpeg_path=ffmpeg_path)
+    else:
+        audio_directory = dqc['audio_directory']
+
     # Discover available audio.
     audio_collection = AudioCollection(
-        top_dir=dqc['audio_directory'],
+        top_dir=audio_directory,
         audio_store_kw_args={'audio_extensions': audio_extensions}
     )
 
@@ -46,7 +75,7 @@ def main():
     bot_helper = BotHelper(audio_collection=audio_collection, aliases=aliases)
 
     # Instantiate a DisQuipBot.
-    bot = DisQuipBot(bot_helper=bot_helper, ffmpeg=dqc['ffmpeg_path'],
+    bot = DisQuipBot(bot_helper=bot_helper, ffmpeg=ffmpeg_path,
                      cmd_prefix=dqc['cmd_prefix'])
 
     # Run it! This will connect to Discord.
