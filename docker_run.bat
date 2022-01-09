23dfd25d0e23fdd69ca6fc8b2271ecb325fdde3d
Brandon Thayer <brandon.lewis.thayer@gmail.com>
2022-01-09 11:26:58 -0800

    Ripping out Docker stuff, don't want to support it any more

--- docker_run.bat
+++
@@ -1,5 +0,0 @@
-docker run --rm^
- --mount type=bind,source=%CD%/disquip.ini,target=/disquip-bot/disquip.ini^
- --mount type=bind,source=%CD%/audio_files,target=/disquip-bot/audio_files^
- --mount type=bind,source=%CD%/audio_files_normalized,target=/disquip-bot/audio_files_normalized^
- blthayer/disquip-bot:latest
